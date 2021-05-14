da30dbe966eb51de7c93cac9a34aa0166dbdd694
Mark Sherman <marksherman@Marks-iMac.local>
2021-05-14 11:58:17 -0400

    added split function and linked index.html to single-repo.html when user input is added and issue is clicked

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,18 +1,32 @@
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    console.log(repoName);
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+}
+
 var getRepoIssues = function(repo) {
-    console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
     fetch(apiUrl).then(function(response) {
         // request was successful 
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
-                // pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
+            console.log(response);
             alert("There was a problem with your request!");
         }
     });
@@ -21,6 +35,7 @@ var getRepoIssues = function(repo) {
 var displayIssues = function(issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "this repo has no open issues!";
+        return;
     }
     for (var i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
@@ -53,4 +68,17 @@ var displayIssues = function(issues) {
     }
 };   
 
-getRepoIssues("facebook/react");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit";
+    
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
